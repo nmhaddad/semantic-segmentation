6250285cd2c1aa2692c09fcce8401fc765c88f67
Nate Haddad <nhaddad2112@gmail.com>
2025-06-26 17:18:19 -0400

    Cleanup

--- test.py
+++ test.py
@@ -9,20 +9,19 @@ from PIL import Image
 from models import DeepLabWrapper
 from utils import display_example_pair, vis_grid_4x3, vis_segmentation
 
-if __name__ == "__main__":
-    with open("config/config.yaml", "r") as f:
-        config = yaml.safe_load(f)
+with open("config/config.yaml", "r") as f:
+    config = yaml.safe_load(f)
 
-    example_image = Image.open(op.join(config["DATA_PATH"], "train/iid000008/rgb.jpg"))
-    example_mask = Image.open(op.join(config["DATA_PATH"], "train/iid000008/labels.png"))
-    image_display = np.array(example_image)
-    mask_display = np.array(example_mask.convert("RGB"))
-    display_example_pair(image_display, mask_display)
-    w, h, *_ = example_image.size
+example_image = Image.open(op.join(config["DATA_PATH"], "train/iid000008/rgb.jpg"))
+example_mask = Image.open(op.join(config["DATA_PATH"], "train/iid000008/labels.png"))
+image_display = np.array(example_image)
+mask_display = np.array(example_mask.convert("RGB"))
+display_example_pair(image_display, mask_display)
+w, h, *_ = example_image.size
 
-    model = DeepLabWrapper(model_path=config["LOAD_MODEL_PATH"], input_shape=(w, h))
+model = DeepLabWrapper(model_path=config["LOAD_MODEL_PATH"], input_shape=(w, h))
 
-    predicted_masks = model(example_image)
+predicted_masks = model(example_image)
 
-    vis_segmentation(example_image, np.array(predicted_masks))
-    vis_grid_4x3(model, config["DATA_PATH"])
+vis_segmentation(example_image, np.array(predicted_masks))
+vis_grid_4x3(model, config["DATA_PATH"])
--- train.py
+++ train.py
@@ -8,29 +8,28 @@ import yaml
 from models import DeepLabWrapper
 from utils import Trainer, get_dataloader
 
-if __name__ == "__main__":
-    with open("config/config.yaml", "r") as f:
-        config = yaml.safe_load(f)
+with open("config/config.yaml", "r") as f:
+    config = yaml.safe_load(f)
 
-    # create an output directory for the model if one doesn't exist
-    os.makedirs("runs", exist_ok=True)
+# create an output directory for the model if one doesn't exist
+os.makedirs("runs", exist_ok=True)
 
-    # create dataloaders
-    dataloaders = get_dataloader(
-        config["DATA_PATH"], batch_size=config["BATCH_SIZE"], resize_shape=(config["IMG_HEIGHT"], config["IMG_WIDTH"])
-    )
+# create dataloaders
+dataloaders = get_dataloader(
+    config["DATA_PATH"], batch_size=config["BATCH_SIZE"], resize_shape=(config["IMG_HEIGHT"], config["IMG_WIDTH"])
+)
 
-    # create the model
-    model = DeepLabWrapper(backbone=config["BACKBONE"], num_mask_channels=config["NUM_MASK_CHANNELS"])
+# create the model
+model = DeepLabWrapper(backbone=config["BACKBONE"], num_mask_channels=config["NUM_MASK_CHANNELS"])
 
-    # train the model
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters, lr=1e-4)
-    trainer = Trainer(
-        model, dataloaders, criterion, optimizer, num_epochs=config["NUM_EPOCHS"], is_inception=config["IS_INCEPTION"]
-    )
-    trainer.train()
+# train the model
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters, lr=1e-4)
+trainer = Trainer(
+    model, dataloaders, criterion, optimizer, num_epochs=config["NUM_EPOCHS"], is_inception=config["IS_INCEPTION"]
+)
+trainer.train()
 
-    # save the model
-    model_path = config.get("SAVE_MODEL_PATH", f"models/{config['BACKBONE']}_v1.{config['NUM_EPOCHS']}.pth")
-    model.save_model(model_path)
+# save the model
+model_path = config.get("SAVE_MODEL_PATH", f"models/{config['BACKBONE']}_v1.{config['NUM_EPOCHS']}.pth")
+model.save_model(model_path)
